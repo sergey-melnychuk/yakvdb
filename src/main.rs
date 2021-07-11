0343b2225e3ee11a48909f031aad2a949a73e026
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-07-12 01:55:14 +0200

    Add draft insert/delete entries r/w using bincode

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::collections::{BTreeMap, HashMap};
 use std::fs;
 use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::{Read, Seek, SeekFrom, Write};
+use std::io::{Read, Seek, SeekFrom, Write, BufReader};
 use std::ops::Bound;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
+use serde::de::DeserializeOwned;
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
 struct FileId(u32);
@@ -24,17 +25,23 @@ impl Location {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct TableEntry {
-    key: Vec<u8>,
-    val: Vec<u8>,
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+enum TableEntry {
+    Insert { key: Vec<u8>, val: Vec<u8> },
+    Remove { key: Vec<u8> },
 }
 
 impl TableEntry {
-    fn new(key: Vec<u8>, val: Vec<u8>) -> Self {
-        Self {
+    fn insert(key: Vec<u8>, val: Vec<u8>) -> Self {
+        Self::Insert {
             key,
             val,
+        }
+    }
+
+    fn remove(key: Vec<u8>) -> Self {
+        Self::Remove {
+            key,
         }
     }
 }
@@ -65,55 +72,39 @@ trait KVIterator {
     fn entries(&self, bound: Bound<&[u8]>) -> dyn Iterator<Item = (Vec<u8>, Vec<u8>)>;
 }
 
-struct InMemory {
-    index: BTreeMap<Vec<u8>, Vec<u8>>,
-}
-
-impl KVStore for InMemory {
-    fn lookup(&mut self, key: &[u8]) -> Option<Vec<u8>> {
-        self.index.get(key).map(|val| val.clone())
-    }
-
-    fn insert(&mut self, key: &[u8], val: &[u8]) {
-        self.index.insert(key.to_vec(), val.to_vec());
-    }
-
-    fn remove(&mut self, key: &[u8]) {
-        self.index.remove(key);
-    }
-}
-
-struct Durable<S: Storage> {
-    index: BTreeMap<Vec<u8>, Location>,
+struct Durable<S: Storage, I: Index> {
     storage: S,
+    index: I,
 }
 
-impl<S: Storage> KVStore for Durable<S> {
+impl<S: Storage, I: Index> KVStore for Durable<S, I> {
     fn lookup(&mut self, key: &[u8]) -> Option<Vec<u8>> {
-        let loc = self.index.get(key)?;
-        self.storage.get(loc).ok()
+        let loc = self.index.lookup(key)?;
+        self.storage.get(&loc).ok()
     }
 
     fn insert(&mut self, key: &[u8], val: &[u8]) {
-        let entry = TableEntry::new(key.to_vec(), val.to_vec());
+        let entry = TableEntry::insert(key.to_vec(), val.to_vec());
         let buf = bincode::serialize(&entry).unwrap();
         let mut loc = self.storage.put(&buf).unwrap();
-        loc.pos += 8*2 + key.len() as u32;
+        loc.pos += 8 + key.len() as u32 + 8 + 4;
         loc.len = val.len() as u32;
-        self.index.insert(key.to_vec(), loc);
+        self.index.insert(key, loc);
     }
 
     fn remove(&mut self, key: &[u8]) {
-        // TODO create TableEntry representing removal of the key
+        let entry = TableEntry::remove(key.to_vec());
+        let buf = bincode::serialize(&entry).unwrap();
+        self.storage.put(&buf).unwrap();
         self.index.remove(key);
     }
 }
 
-impl<S: Storage> Durable<S> {
-    fn new(storage: S) -> Self {
+impl<S: Storage, I: Index> Durable<S, I> {
+    fn new(storage: S, index: I) -> Self {
         Self {
-            index: BTreeMap::new(),
             storage,
+            index,
         }
     }
 }
@@ -121,6 +112,31 @@ impl<S: Storage> Durable<S> {
 trait Storage {
     fn get(&mut self, loc: &Location) -> io::Result<Vec<u8>>;
     fn put(&mut self, val: &[u8]) -> io::Result<Location>;
+}
+
+trait Index {
+    fn lookup(&mut self, key: &[u8]) -> Option<Location>;
+    fn insert(&mut self, key: &[u8], loc: Location);
+    fn remove(&mut self, key: &[u8]);
+}
+
+#[derive(Default)]
+struct InMemory {
+    index: BTreeMap<Vec<u8>, Location>,
+}
+
+impl Index for InMemory {
+    fn lookup(&mut self, key: &[u8]) -> Option<Location> {
+        self.index.get(key).cloned()
+    }
+
+    fn insert(&mut self, key: &[u8], loc: Location) {
+        self.index.insert(key.to_vec(), loc);
+    }
+
+    fn remove(&mut self, key: &[u8]) {
+        self.index.remove(key);
+    }
 }
 
 struct OnDisk {
@@ -144,28 +160,13 @@ impl OnDisk {
             fs::create_dir_all(path)?;
             HashMap::with_capacity(16)
         } else {
-            let existing_files: HashMap<FileId, File> = fs::read_dir(path)?
-                .into_iter()
-                .filter_map(|r| r.ok().map(|d| d.path()))
-                .filter(|p| p.is_file() && p.ends_with(".dat"))
+            OnDisk::list(path, ".dat")?
                 .map(|p| {
-                    let name = p
-                        .file_name()
-                        .unwrap()
-                        .to_str()
-                        .unwrap()
-                        .split(".")
-                        .next()
-                        .unwrap();
-                    let id: u32 = u32::from_str_radix(name, 16).unwrap();
-                    let file = OpenOptions::new().read(true).write(true).open(p).unwrap();
-                    (FileId(id), file)
+                    let id = OnDisk::id(&p);
+                    let file = OnDisk::open(&p).unwrap();
+                    (id, file)
                 })
-                .collect();
-
-            // TODO ingest data from discovered .idx files
-
-            existing_files
+                .collect::<HashMap<_, _>>()
         };
 
         Ok(Self {
@@ -176,6 +177,26 @@ impl OnDisk {
 
     fn name(file_id: &FileId) -> String {
         format!("{:08x}.dat", file_id.0)
+    }
+
+    fn id(path: &PathBuf) -> FileId {
+        let name = path.file_name()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .split(".")
+            .next()
+            .unwrap();
+        let id: u32 = u32::from_str_radix(name, 16).unwrap();
+        FileId(id)
+    }
+
+    fn list(path: &Path, suffix: &str) -> io::Result<impl Iterator<Item=PathBuf>> {
+        let owned_suffix = suffix.to_owned();
+        Ok(fs::read_dir(path)?
+            .into_iter()
+            .filter_map(|r| r.ok().map(|d| d.path()))
+            .filter(move |p| p.is_file() && p.file_name().unwrap().to_str().unwrap().ends_with(&owned_suffix)))
     }
 
     fn append(file: &mut File, val: &[u8]) -> io::Result<u32> {
@@ -189,27 +210,39 @@ impl OnDisk {
         file.read_exact(buf)
     }
 
-    // TODO Impl merging non-active (immutable) files into single one with extra index file next to it
-    fn merge() {
-        todo!()
+    fn open(path: &Path) -> io::Result<File> {
+        let mut opt = OpenOptions::new();
+        let file = if path.exists() {
+            let mut old = opt.read(true).write(true).open(path)?;
+            old.seek(SeekFrom::End(0))?;
+            old
+        } else {
+            let mut new = opt.read(true).write(true).create(true).open(path)?;
+
+            // Placeholder for number of entries in the file - required by bincode to deserialize Vec<*Entry>
+            new.write_all(&[0, 0, 0, 0, 0, 0, 0, 0])?;
+
+            new
+        };
+        Ok(file)
     }
 
     fn file(&mut self, file_id: &FileId) -> io::Result<&mut File> {
         let path = Path::new(&self.base).join(OnDisk::name(file_id));
         if !self.files.contains_key(file_id) {
-            let mut opt = OpenOptions::new();
-            let file = if path.exists() {
-                let mut old = opt.read(true).write(true).open(path)?;
-                old.seek(SeekFrom::End(0))?;
-                old
-            } else {
-                let mut new = opt.read(true).write(true).create(true).open(path)?;
-                new.seek(SeekFrom::Start(0))?;
-                new
-            };
+            let file = OnDisk::open(&path)?;
             self.files.insert(file_id.clone(), file);
         }
         Ok(self.files.get_mut(file_id).unwrap())
+    }
+
+    fn entries<T: DeserializeOwned>(path: &Path, suffix: &str) -> io::Result<impl Iterator<Item=T>> {
+        Ok(OnDisk::list(path, suffix)?
+            .flat_map(|p| {
+                let br = BufReader::new(File::open(p).unwrap());
+                let entries: Vec<T> = bincode::deserialize_from(br).unwrap();
+                entries
+            }))
     }
 }
 
@@ -225,7 +258,7 @@ impl Storage for OnDisk {
         if val.len() > u16::MAX as usize {
             return Err(io::Error::new(
                 io::ErrorKind::Other,
-                format!("too long value (max length is {} bytes)", u16::MAX),
+                format!("value too long (max length is {} bytes)", u16::MAX),
             ));
         }
 
@@ -248,17 +281,25 @@ mod tests {
 
     #[test]
     fn test_table_entry() {
-        let entry = TableEntry::new(
-            b"what is the answer to life?".to_vec(),
-            b"it's 42".to_vec());
+        let key = b"what is the answer to life?";
+        let val = b"it's 42";
+        let entry = TableEntry::insert(key.to_vec(), val.to_vec());
 
         let buf = bincode::serialize(&entry).unwrap();
-        assert_eq!(&buf[8..12], b"what");
-        assert_eq!(&buf[(8 + entry.key.len() + 8)..(8 + entry.key.len() + 8 + 7)], b"it's 42");
-        let e: TableEntry = bincode::deserialize(&buf).unwrap();
+        assert_eq!(&buf[12..16], b"what");
 
-        assert_eq!(e.key, entry.key);
-        assert_eq!(e.val, entry.val);
+        // 8 bytes for key len
+        // 8 bytes for val len
+        // 4 bytes for enum variant (insert/remove)
+        let n = 8 + key.len() + 8 + 4;
+        assert_eq!(&buf[n..(n + 7)], b"it's 42");
+
+        if let TableEntry::Insert { key: k, val: v} = bincode::deserialize(&buf).unwrap() {
+            assert_eq!(k, key);
+            assert_eq!(v, val);
+        } else {
+            unreachable!()
+        }
     }
 
     #[test]
@@ -277,10 +318,11 @@ mod tests {
     #[test]
     fn test_durable() {
         let disk = OnDisk::new("target/db").unwrap();
-        let mut db = Durable::new(disk);
+        let mem = InMemory::default();
+        let mut db = Durable::new(disk, mem);
+
         let key = b"sup, doc?";
         let val = b"not much, thanks";
-
         assert!(db.lookup(key).is_none());
 
         db.insert(key, val);
@@ -288,6 +330,48 @@ mod tests {
 
         db.remove(key);
         assert!(db.lookup(key).is_none());
+    }
+
+    #[test]
+    fn test_table_entries() {
+        let entries = vec![
+            TableEntry::Insert { key: b"qweqwe-asdasd-zxcxzc".to_vec(), val: b"123123-567576-8890890".to_vec() },
+            TableEntry::Insert { key: b"asdasd-qweqwe-zxcxzc".to_vec(), val: b"567576-123123-8890890".to_vec() },
+            TableEntry::Insert { key: b"zxcxzc-qweqwe-asdasd".to_vec(), val: b"8890890-123123-567576".to_vec() },
+            TableEntry::Remove { key: b"asdasd-qweqwe-zxcxzc".to_vec() },
+        ];
+
+        let path = Path::new("target/test_table_entries");
+        if !path.exists() {
+            fs::create_dir_all(path).unwrap();
+        }
+
+        OnDisk::list(path, ".dat").unwrap()
+            .for_each(|d| fs::remove_file(d.as_path()).unwrap());
+
+        let disk = OnDisk::new("target/test_table_entries").unwrap();
+        let mem = InMemory::default();
+        let mut db = Durable::new(disk, mem);
+
+        for e in &entries {
+            match e {
+                TableEntry::Insert { key, val } => db.insert(key, val),
+                TableEntry::Remove { key } => db.remove(key)
+            }
+        }
+
+        {
+            // Manually patch the length of entries vector to deserialize
+            // Bincode writes collection length prefix as u64, does not provide an easy way to just parse stream of entries from file
+            let mut f = OpenOptions::new().write(true).open(Path::new("target/test_table_entries/00000000.dat")).unwrap();
+            f.seek(SeekFrom::Start(0)).unwrap();
+            f.write_all(&[entries.len() as u8]).unwrap();
+        }
+
+        let deserialized: Vec<TableEntry> = OnDisk::entries(path, ".dat").unwrap()
+            .into_iter().collect();
+
+        assert_eq!(deserialized, entries);
     }
 }
 
