35b26945f5c8d59f9aac53ef078f0a54f3cbd265
sergey-melnychuk <sergey-melnychuk@users.noreply.github.com>
2021-07-30 13:30:52 +0200

    Add Error.cause() impl for Error::IO error

--- src/api/error.rs
+++ src/api/error.rs
@@ -14,14 +14,24 @@ pub type Result<T> = result::Result<T, Error>;
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::IO(io) => write!(f, "IO error: '{}'.", io),
-            Error::Tree(id, msg) => write!(f, "Tree error (page: {}): '{}'.", id, msg),
-            Error::Other(msg) => write!(f, "Other error: '{}'.", msg),
+            Error::IO(io) =>
+                write!(f, "IO error: '{}'.", io),
+            Error::Tree(id, msg) =>
+                write!(f, "Tree error (page: {}): '{}'.", id, msg),
+            Error::Other(msg) =>
+                write!(f, "Other error: '{}'.", msg),
         }
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Error {
+    fn cause(&self) -> Option<&dyn std::error::Error> {
+        match self {
+            Error::IO(io) => Some(io),
+            _ => None
+        }
+    }
+}
 
 impl From<io::Error> for Error {
     fn from(e: io::Error) -> Self {
